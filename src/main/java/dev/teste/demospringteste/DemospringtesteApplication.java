2ee16efe76ec718bb9c412fd75e7d7408e9a2039
M Tomain <mtomain3@gmail.com>
2025-03-23 16:08:20 -0300

    first commit

---
+++ src/main/java/dev/teste/demospringteste/DemospringtesteApplication.java
@@ -0,0 +1,13 @@
+package dev.teste.demospringteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringtesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringtesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/teste/demospringteste/DemospringtesteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.teste.demospringteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringtesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
